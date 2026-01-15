18751c2e6d46ec175e53d024a8a64f2a544f8033
rehanrehman389 <rehanrehman389@gmail.com>
2026-01-15 12:13:52 +0000

    dynamic instructions

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import { Agent, run } from '@openai/agents'
 
+const location = 'USA';
+
 const helloAgent = new Agent({
     name: 'Hello Agent',
-    instructions: 'Always respond in haiku form.',
+    instructions: function() {
+        if (location === 'India') {
+            return 'Always say namaste and Always respond in haiku form.'
+        } else {
+            return 'Always say hello and Always respond in haiku form.'
+        }
+    },
 });
 
 run(helloAgent, 'My name is Rehan')
